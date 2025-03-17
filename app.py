4cf5e73e65df438995e7708df364cce3823bce07
Dannyz817 <machadodanny.817@gmail.com>
2025-03-17 16:58:48 -0300

    Correções para deploy no Render

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
